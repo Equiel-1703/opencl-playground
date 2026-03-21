54028d7d551ab65fb30e1142ca3f6c111d44a2ad
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-03-21 17:37:52 -0300

    Add CPU and GPU selection logic in heterogeneous.cpp

--- heterogeneous/heterogeneous.cpp
+++ heterogeneous/heterogeneous.cpp
@@ -27,6 +27,14 @@ std::string getKernelCode(const char *file_name)
 
 int main()
 {
+    // CPU and GPU OpenCL vars
+    cl::Platform cpu_plat, gpu_plat;
+    cl::Device cpu, gpu;
+
+    // Flags
+    bool is_cpu_set = false;
+    bool is_gpu_set = false;
+
     // Get available platforms
     std::vector<cl::Platform> available_platforms;
     cl::Platform::get(&available_platforms);
@@ -45,7 +53,7 @@ int main()
     }
     std::cout << std::endl;
 
-    // Printing available devices in each platform
+    // Printing available devices in each platform and selecting the CPU and GPU
     std::vector<cl::Device> available_devices;
     for (auto &p : available_platforms)
     {
@@ -59,8 +67,39 @@ int main()
         for (auto &d : available_devices)
         {
             std::cout << "\t- " << d.getInfo<CL_DEVICE_NAME>() << std::endl;
+
+            if (d.getInfo<CL_DEVICE_TYPE>() == CL_DEVICE_TYPE_CPU && !is_cpu_set)
+            {
+                cpu = d;
+                cpu_plat = p;
+                is_cpu_set = true;
+            }
+            else if (d.getInfo<CL_DEVICE_TYPE>() == CL_DEVICE_TYPE_GPU && !is_gpu_set)
+            {
+                gpu = d;
+                gpu_plat = p;
+                is_gpu_set = true;
+            }
         }
         std::cout << std::endl;
+    }
+
+    if (is_cpu_set)
+    {
+        std::cout << "CPU selected: '" << cpu.getInfo<CL_DEVICE_NAME>() << "' from '" << cpu_plat.getInfo<CL_PLATFORM_NAME>() << "' platform." << std::endl;
+    }
+    else
+    {
+        std::cerr << "Could not find any CPU device in the available platforms." << std::endl;
+    }
+
+    if (is_gpu_set)
+    {
+        std::cout << "GPU selected: '" << gpu.getInfo<CL_DEVICE_NAME>() << "' from '" << gpu_plat.getInfo<CL_PLATFORM_NAME>() << "' platform." << std::endl;
+    }
+    else
+    {
+        std::cerr << "Could not find any GPU device in the available platforms." << std::endl;
     }
 
     return EXIT_SUCCESS;
